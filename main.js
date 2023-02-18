6afcf7f3aef67aedaf7150c07037bab1b7e25b9a
snezk456 <86143353+snezk456@users.noreply.github.com>
2023-02-18 14:29:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+song1 = "";
+song2 = "";
+ function preload() {
+    song1 = loadSound("sound1.mp3");
+    song2 = loadSound("sound2.mp3");
+ }
+ function draw() {
+    image(video, 0, 0, 600, 500);
+ }
+ function setup() {
+    canvas = createCanvas(600, 500);
+    canvas.center;
+
+    video = createCapture(VIDEO);
+    video.hide;
+ }
